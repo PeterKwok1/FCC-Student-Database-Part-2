78b1117f69fef7eb381acc09489864e09fca5d9b
Peter Kwok <peter.kwok94@gmail.com>
2024-01-10 14:26:05 -0500

    Save progress: 2310.1

--- student_info.sh
+++ student_info.sh
@@ -30,3 +30,4 @@ echo -e "\nMajor ID, total number of students in a column named 'number_of_stude
 echo "$($PSQL "select major_id, count(*) as number_of_students, round(avg(gpa), 2) as averag_gpa from students group by major_id having count(*) > 1")"
 
 echo -e "\nList of majors, in alphabetical order, that either no student is taking or has a student whose first name contains a case insensitive 'ma':"
+echo "$($PSQL "select major from students full join majors on students.major_id = majors.major_id where first_name is null or first_name ilike '%ma%' order by major")"
